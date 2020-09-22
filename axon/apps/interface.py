c5bfaeb1d4c9a6468864ee4cf7f02e0872b1396b
Vipin Sharma <sh.vipin@gmail.com>
2020-09-21 17:24:15 -0700

    [lydian]: Expose InterfaceManager::get_interface()
    Changes to support InterfaceManager::get_interface() to give interface
    based on interface name and IP address associated. There can be more
    than one IP address associated with an interface.
    
    Change-Id: Ie110071a94b1f341c25ecb6f07c86062c221698f
    author: Gavin Chang <cgavin@vmware.com>

---
+++ axon/apps/interface.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2 License
+# The full license information can be found in LICENSE.txt
+# in the root directory of this project.
+
+from axon.utils.network_utils import InterfaceManager
+
+
+class InterfaceApp(object):
+    def __init__(self):
+        self._if_mngr = InterfaceManager()
+
+    def list_interfaces(self):
+        return self._if_mngr.get_all_interfaces()
+
+    def get_interface(self, name, ip=None):
+        return self._if_mngr.get_interface(name, ip)
