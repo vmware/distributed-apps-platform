4f2e0f0f22d73efc8d712ad70ba06a61ede9e42f
Vipin Sharma <sh.vipin@gmail.com>
2020-09-25 10:25:43 -0700

    lydian: Unit test for config app
    Changes include:
        - Use sql30 0.1.3 feature to avoid repeat passing self.TABLE in each
        call.
        - Make database file name configurable.
        - Add unit test case for the Config app.
    
    jira: #NPTF-2483
    Change-Id: I9b07833e4360c05a9fc074f9f1afce100eaed421

--- axon/apps/config.py
+++ axon/apps/config.py
@@ -5,6 +5,7 @@
 # in the root directory of this project.
 
 import logging
+import os
 from sql30 import db
 
 
@@ -16,10 +17,10 @@ configs = None
 
 class Config(db.Model, BaseApp):
     NAME = "CONFIG"
-
+    DB_NAME = 'config.db'
     TABLE = 'config'
     DB_SCHEMA = {
-        'db_name': 'config.db',
+        'db_name': DB_NAME,
         'tables': [
             {
                 'name': TABLE,
@@ -31,17 +32,37 @@ class Config(db.Model, BaseApp):
         }
     VALIDATE_BEFORE_WRITE = True
 
+<<<<<<< HEAD
     def __init__(self):
         super(Config, self).__init__()
+=======
+    DEFAULT_CONFIG = '/etc/axon/axon.conf'
+
+    def __init__(self, db_file=None):
+        # Set database name.
+        db_name = db_file or self.DB_NAME
+        super(Config, self).__init__(db_name=db_name)
+        self.table = self.TABLE
+
+        # set params
+>>>>>>> 9e6d7e7... axon: Unit test for config app
         self._params = {}
+
+        # Read configs, load from db file.
         self._read_config()
         self._update_from_db()
         self.save_params()
 
     def _read_config(self):
         configs = []
+<<<<<<< HEAD
         with open('/etc/axon/axon.conf', 'r') as fp:
             configs = fp.readlines()
+=======
+        if os.path.exists(self.DEFAULT_CONFIG):
+            with open(self.DEFAULT_CONFIG, 'r') as fp:
+                configs = fp.readlines()
+>>>>>>> 9e6d7e7... axon: Unit test for config app
 
         for config in configs:
             config = config.strip()
@@ -56,7 +77,22 @@ class Config(db.Model, BaseApp):
         Local database / persisted file is the source of truth.
         It will override the values read from config file.
         """
+<<<<<<< HEAD
         pass
+=======
+        configs = self.read()
+        for key, val in configs:
+            self._params[key] = val
+
+    def save_to_db(self):
+        """
+        Save config params in local cache to database file.
+        """
+        for param, val in self._params.items():
+            self._persist_param(param, val)
+
+        self.commit()
+>>>>>>> 9e6d7e7... axon: Unit test for config app
 
     def get_param(self, param):
         if param in self._params:
@@ -74,10 +110,22 @@ class Config(db.Model, BaseApp):
         if write_to_db:
             self.write(self.TABLE, param=param, value=val)
 
+<<<<<<< HEAD
     def save_params(self):
         for key, val in self._params.items():
             self.write(self.TABLE, param=key, value=val)
         self.commit()
+=======
+    def _persist_param(self, param, val):
+        """
+        Sets a param, val in database file.
+        """
+        record = self.read(param=param)
+        if record:
+            self.update(condition={'param':param}, value=val)
+        else:
+            self.write(param=param, value=val)
+>>>>>>> 9e6d7e7... axon: Unit test for config app
 
 
 def _get_configs():
---
+++ axon/tests/apps/test_config.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# Copyright (c) 2020 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2 License
+# The full license information can be found in LICENSE.txt
+# in the root directory of this project.
+
+import logging
+import os
+import sqlite3
+import unittest
+import uuid
+
+from axon.apps.config import Config
+
+
+DB_NAME = 'config_test.db'
+
+log = logging.getLogger(__name__)
+
+
+class ConfigAppTest(unittest.TestCase):
+    DUMMY_DATA = {
+        'param1': 'val1',
+        'param2': 'val2',
+        'param3': 'val3'
+        }
+
+    def setUp(self):
+        if os.path.exists(DB_NAME):
+            os.remove(DB_NAME)
+
+    def _get_new_app(self):
+        return Config(db_file=DB_NAME)
+
+    def test_add(self):
+        self.app = self._get_new_app()
+
+        # TEST : Adding params in Config
+        for param, val in self.DUMMY_DATA.items():
+            self.app.set_param(param=param, val=val)
+
+        # TEST : Adding same values would lead to a warning
+        # but no assertion / exception to break the run.
+        for param, val in self.DUMMY_DATA.items():
+            self.app.set_param(param=param, val=val)
+
+        self.app.save_to_db()
+        self.app.close()
+
+        # Get New instance of app()
+        # If persistence works, all the rules must be brought up fine.
+        self.app = self._get_new_app()
+
+        # TEST : All the params must be present.
+        for param, val in self.DUMMY_DATA.items():
+            self.assertEqual(val, self.app.get_param(param))
+
+    def tearDown(self):
+        os.remove(DB_NAME)
