75a0c0b059749722e0662e3fd982c0667cf10a7d
Vipin Sharma <sh.vipin@gmail.com>
2020-10-19 19:49:13 -0700

    lydian: Expose iperf, packet capture , resource monitoring apps
    Changes to :
        - Expose iperf, packet capture , resource monitoring apps.
        - Podium app to expose all the apps.
    
    jira: #NPTF-2537
    
    Change-Id: Iae8fa21af8ca27a20eb12ba33f054fff42a55ebd

--- lydian/apps/podium.py
+++ lydian/apps/podium.py
@@ -21,6 +21,12 @@ log =logging.getLogger(__name__)
 _podium = None
 
 
+def _get_host_ip(host, func_ip=None):
+    func = lambda vm: vm.ip
+    func_ip = func if func_ip is None else func_ip
+    return func_ip(host)
+
+
 @exposify
 class Podium(BaseApp):
 
@@ -128,6 +134,98 @@ class Podium(BaseApp):
 
             with LydianClient(srchost) as sclient:
                 sclient.controller.register_traffic([rule])
+
+    def start_pcap(host, pcap_file_name, interface, pcap_args='', func_ip=None):
+        """
+        Starts packet capture on a requested host.
+        """
+        with LydianClient(_get_host_ip(host, func_ip)) as client:
+            client.pcap.start_pcap(pcap_file_name, interface, pcap_args)
+
+
+    def stop_pcap(host, pcap_file_name, func_ip=None):
+        """
+        Starts packet capture on a requested host.
+        """
+        with LydianClient(_get_host_ip(host, func_ip)) as client:
+            client.pcap.stop_pcap(pcap_file_name)
+
+
+    def start_resource_monitoring(host, func_ip=None):
+        """
+        Starts packet capture on a requested host.
+        """
+        with LydianClient(_get_host_ip(host, func_ip)) as client:
+            client.monitor.start()
+
+
+    def stop_resource_monitoring(host, func_ip=None):
+        """
+        Starts packet capture on a requested host.
+        """
+        with LydianClient(_get_host_ip(host, func_ip)) as client:
+            client.monitor.stop()
+
+    def run_iperf3(src, dst, duration=10, udp=False, bandwidth=None,
+                   client_args='', server_args='', func_ip=None, dst_data_nic=None, ipv6=False):
+        """
+        Run iperf between <src> and <dst> over TCP/UDP for <duration> seconds
+
+        Returns iperf client output
+
+        NOTE: By default the transfer rate is unlimited which consumes high CPU
+
+        Parameters
+        ----------
+        src: host
+            iperf client
+        dst: host
+            iperf server
+        duration: int
+            How long iperf should run
+        udp: bool
+            Whether to run in UDP mode or TCP (default: TCP)
+        bandwidth: int
+            Limit traffic to this many Mbits/second
+        client_args: str
+            Additional cli options supported by iperf client
+        server_args: str
+            Additional cli options supported by iperf server
+        func_ip: func
+            functor to get IP address from host object if 'ip' attribute doesn't exist in it.
+        dst_data_nic: str
+            Name of the network inteface associated with iperf server
+        ipv6: bool
+        """
+        src_ip = _get_host_ip(src, func_ip=func_ip)
+        dst_ip = _get_host_ip(dst, func_ip=func_ip)
+        with LydianClient(dst_ip) as server:
+            with LydianClient(src_ip) as client:
+                try:
+                    port = server.iperf.start_iperf_server(args=server_args)
+                    log.info('iperf server: %s is running on port %s', dst_ip, port)
+                    if dst_data_nic:
+                        dst_data_ip = dst.data_ipv6s.get(dst_data_nic, None) if ipv6 \
+                            else dst.data_ips.get(dst_data_nic, None)
+                        assert dst_data_ip, "Unable to retrieve data_ip of %s" % dst.name
+                    else:
+                        dst_data_ip = dst_ip
+                    job_id = client.iperf.start_iperf_client(dst_data_ip, port, duration, udp, bandwidth,
+                                                             args=client_args)
+                    job_info = client.iperf.get_client_job_info(job_id)
+                    log.info('cmd: %s on iperf client running with job id: %d', job_info['cmd'],
+                             job_id)
+                    time.sleep(duration)
+                    while job_info['state'] == 'running':
+                        time.sleep(1)
+                        job_info = client.iperf.get_client_job_info(job_id)
+                        log.info('iperf client job: %d info %s', job_id, job_info)
+                    return job_info['result']
+                finally:
+                    if port:
+                        server.iperf.stop_iperf_server(port)
+                    if job_id:
+                        client.iperf.stop_iperf_client(job_id)
 
 
 def get_podium():
