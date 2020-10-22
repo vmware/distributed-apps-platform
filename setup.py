b31e6347143d0dc13ff738afe4dd920be485d120
Vipin Sharma <sh.vipin@gmail.com>
2020-10-22 13:31:31 -0700

    lydian: Add setup file and version info
    Changes to add setup file and version info.
    
    jira: #NPTF-2539
    
    Change-Id: I465ad3e6fbfcaece72599b3c8b062c85b727aef1

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = {}
+with open("lydian/__init__.py") as fp:
+    exec(fp.read(), version)
+
+
+setuptools.setup(
+    name="lydian",
+    version=version['__version__'],
+    author="Vipin Sharma",
+    author_email="sharmavipin@vmware.com",
+    description="Tool for Traffic Generation, Management and Monitoring",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gitvipin/validation-app-engine",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
