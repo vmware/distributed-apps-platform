ccfcb91704ed2685677c7fcddddd30944d5f829d
Vipin Sharma <sh.vipin@gmail.com>
2020-10-30 15:31:40 -0700

    lydian: Add dependencies in setup.py
    - Bump up sql30 to 0.1.5
    - Add dependencies to setup.py
    
    Change-Id: I66cf86bd573bfddd7202abb24234c750913d20db

--- LYDIAN/setup.py
+++ LYDIAN/setup.py
@@ -9,6 +9,12 @@ version = {}
 with open("lydian/__init__.py") as fp:
     exec(fp.read(), version)
 
+# Dependencies
+deps = []
+with open("requirements.txt") as fp:
+    deps = fp.read().splitlines()
+deps = [x.strip() for x in deps]
+deps = [x for x in deps if x and not x.startswith('#')]
 
 setuptools.setup(
     name="lydian",
@@ -19,6 +25,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gitvipin/validation-app-engine",
+    install_requires=deps,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
