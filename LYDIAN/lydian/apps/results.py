2fef5122a5e1c917f3a9eed624d8b00ba5f5372d
Vipin Sharma <sh.vipin@gmail.com>
2021-01-20 23:22:56 -0800

    lydian: traffic result app fixes.
    Changes:
        - Return traffic results pickled as data can be huge. (Fixes stream not closed error)
        - Coding pattern to ensure context is closed.
        - Add API to return total number of records in traffic table.
    
    jira: #NPTF-2692
    
    Change-Id: Ic86d39cf3c42f02b3659356c7b6f8af28a4ba124

--- LYDIAN/lydian/apps/results.py
+++ LYDIAN/lydian/apps/results.py
@@ -6,7 +6,9 @@
 '''
 Generic Results app. Gives results for traffic operations.
 '''
+
 import logging
+import pickle
 
 from lydian.apps.base import BaseApp, exposify
 from lydian.apps.recorder import TrafficRecordDB
@@ -25,8 +27,21 @@ class Results(BaseApp):
                 _filter[key] = value
             else:
                 log.info("Skipping invalid TrafficRecord key:%s", key)
+
+        result = []
         with TrafficRecordDB() as db:
-            return db.read(tbl=db.TABLE, reqid=reqid, **_filter)
+            result = db.read(tbl=db.TABLE, reqid=reqid, **_filter)
+
+        return pickle.dumps(result)
+
+    def traffic_records_count(self, **kwargs):
+        """
+        Returns total number of records in traffic database.
+        """
+        count = None
+        with TrafficRecordDB() as db:
+            count = db.count(tbl=db.TABLE, **kwargs)
+        return count
 
     def get_latency_stat(self, reqid, method, **kwargs):
         _filter = {}
@@ -35,15 +50,19 @@ class Results(BaseApp):
                 _filter[key] = value
             else:
                 log.info("Skipping invalid TrafficRecord key:%s", key)
+
+        result = None
         with TrafficRecordDB() as db:
             if method == 'avg':
-                return db.avg(field='latency', tbl=db.TABLE, reqid=reqid, **_filter)
+                result =  db.avg(field='latency', tbl=db.TABLE, reqid=reqid, **_filter)
             elif method == 'min':
-                return db.min(field='latency', tbl=db.TABLE, reqid=reqid, **_filter)
+                result = db.min(field='latency', tbl=db.TABLE, reqid=reqid, **_filter)
             elif method == 'max':
-                return db.max(field='latency', tbl=db.TABLE, reqid=reqid, **_filter)
+                result = db.max(field='latency', tbl=db.TABLE, reqid=reqid, **_filter)
             else:
                 log.error("Invalid method: %s for getting latency stat.", method)
+
+        return result
 
     def get_avg_latency(self, reqid, **kwargs):
         return self.get_latency_stat(reqid, method='avg', **kwargs)
